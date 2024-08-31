154054a2a9c0e5561e66c8055bc1ebd475e0e13b
shrey joshi <113532718+Shreyyy07@users.noreply.github.com>
2024-08-31 20:39:09 +0530

    Added summery Generated from AI

--- resume-builder (Frontend)/src/Dashboard/resume/components/forms/Summery.jsx
+++ resume-builder (Frontend)/src/Dashboard/resume/components/forms/Summery.jsx
@@ -6,8 +6,9 @@ import { useParams } from 'react-router-dom';
 import GlobalApi from '../../../../../service/GlobalApi';
 import { Brain, LoaderCircle } from 'lucide-react';
 import { toast } from 'sonner';
+import { AIChatSession } from '../../../../../service/AIModal';
 
-
+const prompt="Job Title: {jobTitle} , Depends on job title give me list of summery for 3 experience level, Mid Level and Freasher level in 3 -4 lines in array format, With summery and experience_level Field in JSON Format"
 function Summery({enabledNext}) {
     const {resumeInfo,setResumeInfo}=useContext(ResumeInfoContext);
     const [summery,setSummery]=useState();
@@ -21,6 +22,17 @@ function Summery({enabledNext}) {
         })
     },[summery])
 
+    const GenerateSummeryFromAI=async()=>{
+        setLoading(true)
+        const PROMPT=prompt.replace('{jobTitle}',resumeInfo?.jobTitle);
+        console.log(PROMPT);
+        const result=await AIChatSession.sendMessage(PROMPT);
+        console.log(JSON.parse(result.response.text()))
+       
+        setAiGenerateSummeryList(JSON.parse([result.response.text()]))
+        setLoading(false);
+    }
+    
     const onSave=(e)=>{
         e.preventDefault();
        
@@ -39,7 +51,7 @@ function Summery({enabledNext}) {
             setLoading(false);
         })
     }
-
+   
 
   return (
     <div>
@@ -67,6 +79,19 @@ function Summery({enabledNext}) {
             </div>
         </form>
         </div>
+
+        {aiGeneratedSummeryList&& <div className='my-5'>
+            <h2 className='font-bold text-lg'>Suggestions</h2>
+            {aiGeneratedSummeryList?.map((item,index)=>(
+                <div key={index} 
+                onClick={()=>setSummery(item?.summary)}
+                className='p-5 shadow-lg my-4 rounded-lg cursor-pointer'>
+                    <h2 className='font-bold my-1 text-primary'>Level: {item?.experience_level}</h2>
+                    <p>{item?.summary}</p>
+                </div>
+            ))}
+        </div>}
+
         </div>
 
   )
